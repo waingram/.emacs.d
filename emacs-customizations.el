4408c9438c1f888353f10b0f6253fb916f18052b
$GIT_AUTHOR_NAME <$GIT_AUTHOR_EMAIL>
2013-02-21 15:02:04 -0600

    turn on column number mode; turn off tab indent mode

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -3,10 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(current-language-environment "UTF-8")
  '(custom-enabled-themes (quote (wombat)))
  '(ido-enable-flex-matching t)
  '(ido-mode (quote both) nil (ido))
+ '(indent-tabs-mode nil)
  '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("marmalade" . "http://marmalade-repo.org/packages/") ("ELPA" . "http://tromey.com/elpa/") ("melpa" . "http://melpa.milkbox.net/packages/"))))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
